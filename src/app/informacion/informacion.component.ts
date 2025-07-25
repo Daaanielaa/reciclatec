16bed370de7c302eb322a13995a2bc3e24887ecc
Mishel9 <bmc.guaman@yavirac.edu.ec>
2025-07-24 20:50:05 -0500

    cambios

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -3,9 +3,40 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-informacion',
-  standalone: true,             // Cambia a true para standalone
-  imports: [CommonModule],      // Importa CommonModule para directivas comunes
-  templateUrl: './informacion.component.html',
-  styleUrls: ['./informacion.component.css']  // corregí styleUrl a styleUrls
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'informacion.component.html',
+  styleUrls: ['./informacion.component.css']
 })
-export class InformacionComponent { }
+export class InformacionComponent {
+  residuos = [
+    {
+      nombre: 'Teléfono',
+      imagen: 'assets/img/telefono.png',
+      descripcion: 'Dispositivo móvil que contiene múltiples materiales reciclables.',
+      caracteristicas: [
+        'Plástico: 40%',
+        'Vidrio y Cerámica: 20%',
+        'Cobre: 10%',
+        'Níquel: 2% - 10%',
+      ]
+    },
+    {
+      nombre: 'Televisor',
+      imagen: 'assets/img/televisor.png',
+      descripcion: 'Dispositivo para visualización que también contiene metales y plásticos.',
+      caracteristicas: [
+        'Hierro: 30%',
+        'Aluminio: 15%',
+        'Cobre: 10%',
+        'Plástico: 28%',
+      ]
+    }
+  ];
+
+  residuosSeleccionada = this.residuos[0];
+
+  seleccionarResiduo(residuo: any) {
+    this.residuosSeleccionada = residuo;
+  }
+}
